ca6baba42d8344e709f6c0fbdfb2c509da0d115a
novannp <novangarut@gmail.com>
2023-07-29 01:49:43 +0700

    fix module tile on dark mode

--- lib/src/presentation/pages/materi_detail_page.dart
+++ lib/src/presentation/pages/materi_detail_page.dart
@@ -87,6 +87,7 @@ class _MateriDetailPageState extends State<MateriDetailPage> {
                         ? Html(data: widget.materis[index].summary)
                         : const SizedBox(),
                     ExpansionTile(
+                      initiallyExpanded: true,
                       title: const Text(
                         'Modul',
                         style: TextStyle(
@@ -115,81 +116,79 @@ class _MateriDetailPageState extends State<MateriDetailPage> {
         final mod = widget.materis[index].modules![i];
         switch (mod.modname) {
           case 'assign':
-            return Column(
-              children: [
-                ListTile(
-                  onTap: () {},
-                  leading: const Icon(
-                    Icons.assignment,
-                    size: 40,
-                    color: Colors.amber,
+            return Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    onTap: () {},
+                    leading: const Icon(
+                      Icons.assignment,
+                      size: 40,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      mod.name!.decodeHtml(),
+                    ),
                   ),
-                  title: Text(
-                    mod.name!.decodeHtml(),
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ),
-                mod.description != null
-                    ? Html(data: mod.description!)
-                    : const SizedBox()
-              ],
+                  mod.description != null
+                      ? Html(data: mod.description!)
+                      : const SizedBox()
+                ],
+              ),
             );
           case 'forum':
-            return Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey.shade100,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
+            return Card(
               child: ListTile(
                 onTap: () {},
                 leading:
                     const Icon(Icons.forum, size: 40, color: Colors.orange),
                 title: Text(
                   mod.name!.decodeHtml(),
-                  style: Theme.of(context).textTheme.labelMedium,
                 ),
               ),
             );
           case 'lesson':
-            return ListTile(
-              onTap: () {},
-              leading: mod.modicon != null
-                  ? SizedBox(
-                      height: 40,
-                      width: 40,
-                      child: Icon(
-                        Icons.play_lesson,
-                        size: 40,
-                        color: Colors.indigo,
-                      ))
-                  : const Icon(Icons.file_copy, size: 40),
-              title: Text(
-                mod.name!.decodeHtml(),
-                style: Theme.of(context).textTheme.labelMedium,
+            return Card(
+              child: ListTile(
+                onTap: () {},
+                leading: mod.modicon != null
+                    ? const SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: Icon(
+                          Icons.play_lesson,
+                          size: 40,
+                          color: Colors.indigo,
+                        ))
+                    : const Icon(Icons.file_copy, size: 40),
+                title: Text(
+                  mod.name!.decodeHtml(),
+                ),
               ),
             );
           case 'resource':
-            return ListTile(
-              onTap: () {
-                FunctionHelper.downloadFileHandler(
-                    context,
-                    mod.contents?[0].filename ?? '',
-                    (mod.contents?[0].fileurl ?? '')
-                        .replaceAll('?forcedownload=1', ''));
-              },
-              trailing: Text(
-                  '${mod.contents![0].filesize?.formatFileSize() ?? '-'} '),
-              leading: mod.modicon != null
-                  ? SizedBox(
-                      height: 40,
-                      width: 40,
-                      child: SvgPicture.asset('assets/img/file.svg'))
-                  : const Icon(Icons.file_copy, size: 40),
-              title: Text(
-                mod.name!.decodeHtml(),
-                style: Theme.of(context).textTheme.labelMedium,
+            return Card(
+              child: ListTile(
+                subtitle: Text(
+                    '${mod.contents![0].filesize?.formatFileSize() ?? '-'} '),
+                trailing: IconButton.filled(
+                    onPressed: () {
+                      FunctionHelper.downloadFileHandler(
+                          context,
+                          mod.contents?[0].filename ?? '',
+                          (mod.contents?[0].fileurl ?? '')
+                              .replaceAll('?forcedownload=1', ''));
+                    },
+                    icon: const Icon(Icons.download)),
+                leading: mod.modicon != null
+                    ? SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: SvgPicture.asset('assets/img/file.svg'))
+                    : const Icon(Icons.file_copy, size: 40),
+                title: Text(
+                  mod.name!.decodeHtml(),
+                ),
               ),
             );
           case 'quiz':
@@ -216,7 +215,6 @@ class _MateriDetailPageState extends State<MateriDetailPage> {
                               const SizedBox(width: 10),
                               Text(
                                 mod.name!.decodeHtml(),
-                                style: Theme.of(context).textTheme.labelMedium,
                               ),
                               const Spacer(),
                               if (mod.uservisible! == true)
@@ -272,138 +270,107 @@ class _MateriDetailPageState extends State<MateriDetailPage> {
               ],
             );
           case 'label':
-            return Container(
-              margin: const EdgeInsets.only(bottom: 8),
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey.shade100,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    mod.name!.decodeHtml(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelMedium!
-                        .copyWith(fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 8),
-                  SizedBox(
-                    height: 25,
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Tandai Selesai',
-                        style: Theme.of(context).textTheme.labelSmall,
-                      ),
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      mod.name!.decodeHtml(),
                     ),
-                  )
-                ],
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      height: 25,
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Tandai Selesai',
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             );
           case 'workshop':
-            return Container(
-              padding: const EdgeInsets.only(top: 8),
-              margin: const EdgeInsets.only(bottom: 8),
-              decoration: BoxDecoration(
-                color: !mod.completiondata!['uservisible']
-                    ? Colors.grey.shade100
-                    : Colors.white,
-                border: Border.all(
-                  color: Colors.grey.shade100,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      log(mod.uservisible!.toString());
-                    },
-                    leading: Icon(
-                      Icons.people_alt_rounded,
-                      size: 40,
-                      color: mod.uservisible! == true
-                          ? Colors.red.shade400
-                          : Colors.grey,
-                    ),
-                    title: Text(
-                      mod.name!.decodeHtml(),
-                      style: Theme.of(context).textTheme.labelMedium,
-                    ),
-                    trailing: !mod.completiondata!['uservisible']
-                        ? const Icon(Icons.lock)
-                        : null,
-                    subtitle: !mod.completiondata!['uservisible']
-                        ? null
-                        : SizedBox(
-                            height: 25,
-                            width: 100,
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Tandai Selesai',
-                                style: Theme.of(context).textTheme.labelSmall,
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        log(mod.uservisible!.toString());
+                      },
+                      leading: Icon(
+                        Icons.people_alt_rounded,
+                        size: 40,
+                        color: mod.uservisible! == true
+                            ? Colors.red.shade400
+                            : Colors.grey,
+                      ),
+                      title: Text(
+                        mod.name!.decodeHtml(),
+                      ),
+                      trailing: !mod.completiondata!['uservisible']
+                          ? const Icon(Icons.lock)
+                          : null,
+                      subtitle: !mod.completiondata!['uservisible']
+                          ? null
+                          : SizedBox(
+                              height: 25,
+                              width: 100,
+                              child: OutlinedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Tandai Selesai',
+                                  style: Theme.of(context).textTheme.labelSmall,
+                                ),
                               ),
                             ),
-                          ),
-                  ),
-                  const SizedBox(height: 8),
-                  Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 4),
-                      decoration: BoxDecoration(
-                        color: !mod.completiondata!['uservisible']
-                            ? Colors.grey.shade200
-                            : Colors.white,
-                        border: Border.all(
-                          color: Colors.grey.shade100,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: mod.dates!.isNotEmpty
-                          ? Column(
-                              children: [
-                                if (mod.dates!.isNotEmpty)
-                                  for (DateModel date in mod.dates!)
-                                    Row(
-                                      children: [
-                                        const Icon(Icons.calendar_month,
-                                            size: 20),
-                                        Text.rich(
-                                          TextSpan(
-                                            text: date.label!,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelMedium!
-                                                .copyWith(
-                                                    fontWeight:
-                                                        FontWeight.w600),
-                                            children: [
-                                              TextSpan(
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .labelMedium,
-                                                text: DateTime
-                                                        .fromMillisecondsSinceEpoch(
-                                                            date.timestamp! *
-                                                                1000)
-                                                    .toString()
-                                                    .formatDate(),
-                                              )
-                                            ],
-                                          ),
+                    ),
+                    const SizedBox(height: 8),
+                    mod.dates!.isNotEmpty
+                        ? Column(
+                            children: [
+                              if (mod.dates!.isNotEmpty)
+                                for (DateModel date in mod.dates!)
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.calendar_month,
+                                          size: 20),
+                                      Text.rich(
+                                        TextSpan(
+                                          text: date.label!,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelMedium!
+                                              .copyWith(
+                                                  fontWeight: FontWeight.w600),
+                                          children: [
+                                            TextSpan(
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelMedium,
+                                              text: DateTime
+                                                      .fromMillisecondsSinceEpoch(
+                                                          date.timestamp! *
+                                                              1000)
+                                                  .toString()
+                                                  .formatDate(),
+                                            )
+                                          ],
                                         ),
-                                      ],
-                                    ),
-                              ],
-                            )
-                          : const SizedBox())
-                ],
+                                      ),
+                                    ],
+                                  ),
+                            ],
+                          )
+                        : const SizedBox()
+                  ],
+                ),
               ),
             );
           default:
